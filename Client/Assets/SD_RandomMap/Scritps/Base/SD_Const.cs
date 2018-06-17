1e4317c7d476ae465dbe9c56cbe34015470a3f7a
TinaClient <40324887+TinaClient@users.noreply.github.com>
2018-06-17 14:56:47 +0800

    基础框架

---
+++ Client/Assets/SD_RandomMap/Scritps/Base/SD_Const.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SuperDreamTools;
+
+namespace SuperDreamTools
+{
+    public class SD_Const
+    {
+
+        
+    }
+}
+
---
+++ Client/Assets/SD_RandomMap/Scritps/Base/SD_MapCell.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SuperDreamTools;
+
+namespace SuperDreamTools
+{
+
+    public class SD_MapCell
+    {
+
+        public SD_MapCell()
+        {
+
+        }
+    }
+
+}
+
